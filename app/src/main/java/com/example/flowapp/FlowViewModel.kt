0ffa60c31690e47c530b08ef362360841697b7b1
gyotro <giovanni.dintrono@gmail.com>
2023-04-20 11:36:26 +0200

    aggiunti state Flow e Shared Flow

--- app/src/main/java/com/example/flowapp/FlowViewModel.kt
+++ app/src/main/java/com/example/flowapp/FlowViewModel.kt
@@ -1,12 +1,46 @@
 package com.example.flowapp
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 class FlowViewModel: ViewModel() {
+
+    // ora proviamo uno state Flow: utile quando dobbiamo memorizzare lo stato di uno screen
+    // da best practice si crea uno stateflow mutabile ed uno immutabile
+
+    // stateFlow e ShardedFlow Sono degli hot flow: se non c'è nessuno che li colletta, vengono persi
+    private val _stateFlow = MutableStateFlow(0) // mutable
+    val stateFlow = _stateFlow.asStateFlow() // aggiungendo asStateFlow() lo si fa immutabile
+
+    // stessa cosa con uno sharedFlow
+    private val _sharedFlow = MutableSharedFlow<Int>(replay = 5) // creazione di uno sharedFlow, con replay (non obbligatorio) creiamo una sorta di cache che ci immagazzina il valore
+    val sharedFlow = _sharedFlow.asSharedFlow() // aggiungendo asStateFlow() lo si fa immutabile
+
+    // gli sharedFlow sono utilizzati per catturare eventi che vanno eseguiti una singola volta,
+    // ad esempio in Compose si possono utilizzare con i LaunchedEffect, che vengono utilizzati una sola volta
+
+/*    LaunchedEffect(key1 = "true") {
+        viewModel.sharedFlow.collect{ number ->
+
+        }
+    }*/
+    fun emitSharedFlow(square: Int) {
+        viewModelScope.launch {
+            _sharedFlow.emit( square * square)
+        }
+    }
+
+    fun incrementStateFlow() {
+        _stateFlow.value += 1
+    }
+
     val startingValue = 10
     // dal flow si emettono dei valori (simile a Mono o Flux in Java Reactive
+    // questo modo di creare i flow è detto cold flow, xkè emette valori ed aspetta che qualcuni li colletti
     val countdownFlow = flow<Int> {
         var currentValue = startingValue
         // viene emesso il valore iniziale
--- app/src/main/java/com/example/flowapp/MainActivity.kt
+++ app/src/main/java/com/example/flowapp/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.flowapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.*
 //import androidx.compose.material.MaterialTheme
 //import androidx.compose.material.Surface
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
@@ -18,30 +19,56 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import com.example.flowapp.ui.theme.FlowAppTheme
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.*
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("FlowOperatorInvokedInComposition", "StateFlowValueCalledInComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FlowAppTheme {
                 val viewModel by viewModels<FlowViewModel>()
                 // raccogliamo le emissioni del Flow
-                val currentValue: State<Int> = viewModel.countdownFlow.collectAsState(viewModel.startingValue)
+                val currentValue: State<Int> = viewModel.countdownFlow
+                    // possiamo fare tutte le classiche operazioni
+                    .filter {
+                    time -> time % 2 == 0
+                }.map { time -> time * 10 }
+                    .onEach {
+                        Log.d("Flow", "printing flow val $it")
+                    }
+                    .collectAsState(viewModel.startingValue)
+                // settiamo il valore dello stateFlow, se non inseriamo collectaAsState il valore non incrementa
+                val count = viewModel.stateFlow.collectAsState(0)
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Box(modifier = Modifier.fillMaxSize()) {
-                        Text(
+                        /*Text(
                             text = currentValue.value.toString(),
-                            style = MaterialTheme.typography.bodyLarge,
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold,
                             color = Color.Black,
-                            modifier = Modifier.align(Alignment.Center)
-                        )
+                            modifier = Modifier
+                                .size(40.dp)
+                                .align(Alignment.Center)
+                        )*/
+                        Button(onClick = { viewModel.incrementStateFlow() },
+                            modifier = Modifier
+                                .align(Alignment.Center))
+                            {
+                                Text(text = "Counter is: " + count.value.toString(),
+                                    style = MaterialTheme.typography.displayMedium
+                                )
+                            }
+
                     }
                 }
             }
