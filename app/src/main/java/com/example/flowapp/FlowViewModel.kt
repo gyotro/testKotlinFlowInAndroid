9599fc8c9cea2d5928424807202497b31772f54d
gyotro <giovanni.dintrono@gmail.com>
2023-04-23 20:40:42 +0200

    test con shared flow su SnackBar

--- app/src/main/java/com/example/flowapp/FlowViewModel.kt
+++ app/src/main/java/com/example/flowapp/FlowViewModel.kt
@@ -4,12 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
-class FlowViewModel: ViewModel() {
+class FlowViewModel : ViewModel() {
 
     // ora proviamo uno state Flow: utile quando dobbiamo memorizzare lo stato di uno screen
     // da best practice si crea uno stateflow mutabile ed uno immutabile
@@ -19,20 +18,21 @@ class FlowViewModel: ViewModel() {
     val stateFlow = _stateFlow.asStateFlow() // aggiungendo asStateFlow() lo si fa immutabile
 
     // stessa cosa con uno sharedFlow
-    private val _sharedFlow = MutableSharedFlow<Int>(replay = 5) // creazione di uno sharedFlow, con replay (non obbligatorio) creiamo una sorta di cache che ci immagazzina il valore
+    private val _sharedFlow =
+        MutableSharedFlow<Int>(replay = 1) // creazione di uno sharedFlow, con replay (non obbligatorio) creiamo una sorta di cache che ci immagazzina il valore
     val sharedFlow = _sharedFlow.asSharedFlow() // aggiungendo asStateFlow() lo si fa immutabile
 
     // gli sharedFlow sono utilizzati per catturare eventi che vanno eseguiti una singola volta,
     // ad esempio in Compose si possono utilizzare con i LaunchedEffect, che vengono utilizzati una sola volta
 
-/*    LaunchedEffect(key1 = "true") {
-        viewModel.sharedFlow.collect{ number ->
+    /*    LaunchedEffect(key1 = "true") {
+            viewModel.sharedFlow.collect{ number ->
 
-        }
-    }*/
+            }
+        }*/
     fun emitSharedFlow(square: Int) {
         viewModelScope.launch {
-            _sharedFlow.emit( square * square)
+            _sharedFlow.emit(square * square)
         }
     }
 
@@ -41,6 +41,7 @@ class FlowViewModel: ViewModel() {
     }
 
     val startingValue = 10
+
     // dal flow si emettono dei valori (simile a Mono o Flux in Java Reactive
     // questo modo di creare i flow è detto cold flow, xkè emette valori ed aspetta che qualcuni li colletti
     val countdownFlow = flow<Int> {
@@ -61,4 +62,23 @@ class FlowViewModel: ViewModel() {
     fun changeLiveData() {
         _liveData.value = "Live Data Modified!!"
     }
+
+    // Test with StateFlow
+    private val _stateFlowTest = MutableStateFlow("Initial_Value") // mutable
+    val stateFlowTest = _stateFlowTest.asStateFlow()
+
+    // Test with SharedFlow
+    private val _sharedFlowTest = MutableSharedFlow<String>(1) // mutable
+    val sharedFlowTest = _sharedFlowTest.asSharedFlow()
+
+    fun changeStateFlow() {
+        _stateFlowTest.value = "State Flow Modified!!"
+    }
+
+    fun snackBarEmit() {
+        viewModelScope.launch() {
+            _sharedFlowTest.tryEmit("Shared Flow Modified!!")
+        }
+    }
+
 }
--- app/src/main/java/com/example/flowapp/MainActivity.kt
+++ app/src/main/java/com/example/flowapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.flowapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.Layout
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,19 +15,17 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.flowapp.ui.theme.FlowAppTheme
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("FlowOperatorInvokedInComposition", "StateFlowValueCalledInComposition")
+    @SuppressLint("FlowOperatorInvokedInComposition", "StateFlowValueCalledInComposition",
+        "CoroutineCreationDuringComposition"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,9 +34,9 @@ class MainActivity : ComponentActivity() {
                 // raccogliamo le emissioni del Flow
                 val currentValue: State<Int> = viewModel.countdownFlow
                     // possiamo fare tutte le classiche operazioni
-                    .filter {
-                    time -> time % 2 == 0
-                }.map { time -> time * 10 }
+                    .filter { time ->
+                        time % 2 == 0
+                    }.map { time -> time * 10 }
                     .onEach {
                         Log.d("Flow", "printing flow val $it")
                     }
@@ -50,35 +47,182 @@ class MainActivity : ComponentActivity() {
                 // il live Data va osservato
                 val liveData by viewModel.liveData.observeAsState()
 
+                // lo state Flow va osservato
+                val stateFlow by viewModel.stateFlowTest.collectAsState(this)
+
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
                     Column(modifier = Modifier.fillMaxSize()) {
                         Text(
-                            text = ( liveData ?: "null" ),
+                            text = liveData.toString() ?: "null",
                             style = MaterialTheme.typography.headlineLarge,
                             fontWeight = FontWeight.Bold,
                             color = Color.Black,
                             modifier = Modifier
-          //                      .size(170.dp)
+                                //                      .size(170.dp)
                                 .padding(20.dp)
                                 .align(Alignment.CenterHorizontally)
                         )
-                        Button(onClick = { viewModel.changeLiveData() },
+                        Button(
+                            onClick = { viewModel.changeLiveData() },
                             modifier = Modifier.align(Alignment.CenterHorizontally)
                                 .padding(end = 20.dp)
+                        )
+                        {
+                            Text(
+                                text = "LiveData",
+                                style = MaterialTheme.typography.displayMedium
                             )
-                            {
-                                Text(text = "LiveData",
-                                    style = MaterialTheme.typography.displayMedium
-                                )
+                        }
+                        Text(
+                            text = stateFlow.toString(),
+                            style = MaterialTheme.typography.headlineLarge,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            modifier = Modifier
+                                //                      .size(170.dp)
+                                .padding(20.dp)
+                                .align(Alignment.CenterHorizontally)
+                        )
+
+
+                        Button(
+                            onClick = { viewModel.changeStateFlow() },
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                                .padding(end = 20.dp)
+                        )
+                        {
+                            Text(
+                                text = "StateFlow",
+                                style = MaterialTheme.typography.displayMedium
+                            )
+                        }
+                        //val snackBarMessage = viewModel.sharedFlowTest
+
+                        val snackbarHostState = remember { SnackbarHostState() }
+                        val scope = rememberCoroutineScope()
+                        var message by remember { mutableStateOf<String>("inizio") }
+
+                        // la coroutine si mette in ascolto della emissione dello shared Flow
+                        scope.launch {
+                            viewModel.sharedFlowTest.collectLatest {
+                                message = it
                             }
+                        }
+
+                        Scaffold(
+                            modifier = Modifier
+                                .padding(40.dp)
+                                .align(Alignment.CenterHorizontally),
+                            snackbarHost = { SnackbarHost(snackbarHostState) },
+                            /*floatingActionButton = {
+                                //var clickCount by remember { mutableStateOf(0) }
+                                ExtendedFloatingActionButton(
+                                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                                    onClick = {
+                                        // show snackbar as a suspend function
+                                        scope.launch {
+                                            viewModel.sharedFlowTest.collectLatest {
+                                                snackbarHostState.showSnackbar(
+                                                    "Snackbar $it"
+                                                )
+                                            }
+
+              *//*                              snackbarHostState.showSnackbar(
+                                                "Snackbar # ${++clickCount}"
+                                            )*//*
+                                        }
+                                        viewModel.sbackBarEmit()
+                                    }
+                                ) { Text("Show snackbar") }
+                            }*/
+                            content = { innerPadding ->
+                                /*Text(
+                                    text = "Click To show Toast App Bar",
+                                    modifier = Modifier.padding(innerPadding).fillMaxSize().wrapContentSize()
+                                )*/
+                                Button(
+                                    onClick = {
+                // va lanciata prima di visualizzare il messaggio (altrimenti al primo avvio non si visualizza nulla)
+                                        viewModel.snackBarEmit()
+                                        scope.launch {
+                                        // show snackbar as a suspend function
+                                         snackbarHostState.showSnackbar(
+                                             duration = SnackbarDuration.Short,
+                                             message = "Snackbar $message"
+                                                )
+                                            }
+                                    }
+
+                                            /*                              snackbarHostState.showSnackbar(
+                                                                              "Snackbar # ${++clickCount}"
+                                                                          )*/
+,
+         //                           modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 40.dp)
+                                       modifier = Modifier.wrapContentSize()
+                                )
+                                {
+                                    Text(
+                                        text = "SharedFlow",
+                                        style = MaterialTheme.typography.displayMedium
+                                    )
+                                }
+                            }
+                        )
+
+
+                       /* Button(
+                            onClick = { viewModel.sbackBarEmit() },
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                                .padding(top = 40.dp)
+                        )
+                        {
+                            Text(
+                                text = "SharedFlow",
+                                style = MaterialTheme.typography.displayMedium
+                            )
+                        }*/
 
                     }
                 }
             }
         }
     }
+
+/*    @Composable
+    fun SnackBarTrigger(viewModel: FlowViewModel) {
+        val snackbarHostState = remember { SnackbarHostState() }
+        val scope = rememberCoroutineScope()
+        LaunchedEffect(key1 = Unit) {
+            viewModel.sharedFlowTest.collectLatest {
+                Scaffold(
+                    snackbarHost = { SnackbarHost(snackbarHostState) },
+                    *//*floatingActionButton = {
+                        var clickCount by remember { mutableStateOf(0) }
+                        ExtendedFloatingActionButton(
+                            onClick = {
+                                // show snackbar as a suspend function
+                                scope.launch {
+                                    snackbarHostState.showSnackbar(
+                                        "Snackbar # ${++clickCount}"
+                                    )
+                                }
+                            }
+                        ) { Text("Show snackbar") }
+                    },*//*
+                    content = { innerPadding ->
+                        Text(
+                            text = it,
+                            modifier = Modifier.padding(innerPadding).fillMaxSize().wrapContentSize()
+                        )
+                    }
+                )
+            }
+        }
+    }*/
+
 }
+
 
 
 @Preview(showBackground = true)
